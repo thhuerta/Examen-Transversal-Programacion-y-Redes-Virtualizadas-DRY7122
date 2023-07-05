ab4c1521aa736f847cfdbb55d051e3a85986bf26
thhuerta <th.huerta@duocuc.cl>
2023-07-05 01:31:07 +0000

    añadiendo script de nombres a repositorio

---
+++ scriptnombres.py
@@ -0,0 +1,6 @@
+primer_integrante = input("introduce el nombre del primer integrante:")
+Segundo_integrante = input ("introduce el nombre del segundo integrante:")
+print ("los integrantes del grupo son:")
+print (primer_integrante)
+print (Segundo_integrante)
+
